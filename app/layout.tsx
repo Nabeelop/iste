c6af039cb2c79d1697374336989745fc0b51af54
Harika-44 <seesaharika@gmail.com>
2026-03-29 20:55:49 +0530

    transcend

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,20 @@ import "./globals.css";
 import NavBar from "@/components/NavBar";
 import ParticlesBackground from "@/components/ParticlesBackground";
 import Footer from "@/components/Footer";
+import { Bebas_Neue, Nunito } from "next/font/google";
+const bebasNeue = Bebas_Neue({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-bebas",
+  display: "swap",
+});
 
+const nunito = Nunito({
+  subsets: ["latin"],
+  weight: ["400", "600", "700", "800", "900"],
+  variable: "--font-nunito",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "ISTE NITK",
